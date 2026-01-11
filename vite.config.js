9aecd1719ad938724d38f7d8bb5da1b7d2c4424d
zloy_pelmenb <GEORGE_HEMINGTON@TUTAMAIL.COM>
2026-01-12 00:24:55 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/repository-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/PokemonStoreRELEASE/' : '/',
 });
